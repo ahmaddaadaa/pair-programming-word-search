90122047a5c12bce5165ae62055ad4b627c6f70a
ahmaddaadaa <ahmadd@uvic.ca>
2023-07-05 14:03:03 -0700

    Done in pair programming by Ahmad and Joshua

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
 
-module.exports = wordSearch
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const transposedMatrix = Array.from({ length: cols }, () => new Array(rows));
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      transposedMatrix[j][i] = matrix[i][j];
+    }
+  }
+
+  return transposedMatrix;
+};
+
+
+const wordSearch = (letters, word) => {
+  if (!letters) {
+    return false;
+  }
+
+  const horizontalJoinOriginal = letters.map(ls => ls.join(''));
+  const horizontalJoinTranspose = transpose(letters).map(ls => ls.join(''));
+
+  const fullMatrix = horizontalJoinOriginal.concat(horizontalJoinTranspose);
+
+  for (l of fullMatrix) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+
+
+
+
+module.exports = wordSearch;
